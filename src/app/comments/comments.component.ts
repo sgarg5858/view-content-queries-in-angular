3b2a5d1b83c2b61e512d0419f228bbe2f912ad34
Sanjay Garg <sgarg5858@gmail.com>
2022-07-27 12:37:48 +0530

    tried viewChild to query comment component but didnt work

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { CommentService } from '../comment.service';
+import { CommentComponent } from '../comment/comment.component';
 
 @Component({
   selector: 'app-comments',
@@ -9,13 +10,14 @@ import { CommentService } from '../comment.service';
 export class CommentsComponent implements OnInit,AfterViewInit {
 
   constructor(public commentService:CommentService,private cd:ChangeDetectorRef) { }
+  @ViewChild(CommentComponent,{static:false}) comment:CommentComponent|undefined;
   
 
   ngOnInit(): void {
     this.commentService.getComments();
   }
   ngAfterViewInit(): void {
-
+  console.log(this.comment);
      
   }
 
