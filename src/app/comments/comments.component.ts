e83f6399dbad54755015ca5e0237ee2f862b7662
Sanjay Garg <sgarg5858@gmail.com>
2022-07-27 12:41:23 +0530

    changing data in ngAfterViewInit & avoiding ExpressionChanged Error

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -20,10 +20,16 @@ export class CommentsComponent implements OnInit,AfterViewInit {
   ngAfterViewInit(): void {
   console.log(this.comment);
   this.comments?.changes.subscribe((comments:QueryList<CommentComponent>)=>{
-    
     this.comment=this.comments?.first;
-    console.log(this.comment);
-   
+
+    //Updating Data
+    if(this.comment)
+    {
+      this.comment.comment.email="Sanjay";
+      //to let angular know to pickup the changes
+      // else we will get the ExpressionChangedAfterItHasBeenCheckedError
+      this.cd.detectChanges();
+    }
   })
      
   }
