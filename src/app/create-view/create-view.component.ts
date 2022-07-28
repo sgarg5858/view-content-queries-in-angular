ea7e5d64c7a9caa104c5ca957411e6dae193b617
Sanjay Garg <sgarg5858@gmail.com>
2022-07-28 12:05:35 +0530

    passing heading via template context & avoid ExpressionChangedAfterItHasBeenChecked

--- src/app/create-view/create-view.component.ts
+++ src/app/create-view/create-view.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild, ViewContainerRef, ViewRef } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, TemplateRef, ViewChild, ViewContainerRef, ViewRef } from '@angular/core';
 
 @Component({
   selector: 'app-create-view',
@@ -10,19 +10,20 @@ export class CreateViewComponent implements OnInit,AfterViewInit {
   @ViewChild('template') template:TemplateRef<any>|undefined;
   @ViewChild('container',{read:ViewContainerRef}) container:ViewContainerRef|undefined;
 
-  constructor() { }
+  constructor(private changeDetector:ChangeDetectorRef) { }
 
   ngOnInit(): void {
   }
   ngAfterViewInit(): void {
      this.approach1();
      this.approach2();
+     this.changeDetector.detectChanges();
   }
   approach1()
   {
     if(this.container && this.template)
     {
-      const view:ViewRef = this.template.createEmbeddedView({});
+      const view = this.template.createEmbeddedView({heading:"Comments"});
       this.container.insert(view);
     }
   }
@@ -30,7 +31,7 @@ export class CreateViewComponent implements OnInit,AfterViewInit {
   {
     if(this.container && this.template)
     {
-      const view:ViewRef = this.container.createEmbeddedView(this.template);
+      const view:ViewRef = this.container.createEmbeddedView(this.template,{heading:"comments"});
     }
   }
 
